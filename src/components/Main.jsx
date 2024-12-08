60a718fab580a3243effcf5a944aa7dba74566af
Isaac <mayne.isaac@gmail.com>
2024-12-08 17:08:18 +0100

    make components

---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+import  Guitar from "./Guitar"
+export default function Main() {
+    return (
+        <main className="container-xl mt-5">
+            <h2 className="text-center">Nuestra Colección</h2>
+
+            <div className="row mt-5"> 
+                <Guitar />
+            </div>
+        </main>
+    )
+}
