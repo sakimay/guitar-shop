64476ad4b475a07e6ea0b2ff631a68c95df68fc7
Isaac <mayne.isaac@gmail.com>
2024-12-08 21:59:42 +0100

    add guitars from db

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,20 @@
-import  Guitar from "./Guitar"
+import Guitar from "./Guitar"
+import { useState } from "react"
+import { db } from "../data/db"
 export default function Main() {
+    const [data, setData] = useState(db)
+
     return (
         <main className="container-xl mt-5">
             <h2 className="text-center">Nuestra Colección</h2>
 
-            <div className="row mt-5"> 
-                <Guitar />
+            <div className="row mt-5">
+                {data.map((guitar) => (
+                    <Guitar 
+                        key={guitar.id}
+                        guitar={guitar}
+                    />
+                ))}
             </div>
         </main>
     )
