72dd69f1fe67fbcc6d76ad08995910c88700649a
Isaac <mayne.isaac@gmail.com>
2024-12-14 18:09:25 +0100

    add initial local storage cart

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,13 @@ import { useState, useEffect } from "react"
 import { db } from "./data/db"
 
 function App() {
+
+  const initialCart = () => {
+    const localStorageCart = localStorage.getItem('cart')
+    return localStorageCart ? JSON.parse(localStorageCart) : []
+  }
   const [data, setData] = useState(db)
-  const [cart, setCart] = useState([])
+  const [cart, setCart] = useState(initialCart)
   const MAX_ITEMS = 5
   const MIN_ITEMS = 1
 
